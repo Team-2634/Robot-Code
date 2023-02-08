e61511c7a8df45b42d3bcade6dbb1481436a1348
Crimson <121061785+Adri-nK@users.noreply.github.com>
2023-02-08 14:44:35 -0500

    navx

--- Timed/TimedRizz/src/main/java/frc/robot/Robot.java
+++ Timed/TimedRizz/src/main/java/frc/robot/Robot.java
@@ -9,7 +9,6 @@ package frc.robot;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
