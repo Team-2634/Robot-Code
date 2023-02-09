48e9884f38514280c098eaabad965d2e2f716428
Gryph 2634 <gryphons2634@gmail.com>
2023-02-09 16:54:11 -0500

    fdgj

--- Timed/TimedRizz/src/main/java/frc/robot/Robot.java
+++ Timed/TimedRizz/src/main/java/frc/robot/Robot.java
@@ -29,229 +29,199 @@ import edu.wpi.first.wpilibj.SPI;
 
 public class Robot extends TimedRobot {
 
-  boolean True = true;
-  private final WPI_TalonFX leftFront = new WPI_TalonFX(1);
-  private final WPI_TalonFX leftBack = new WPI_TalonFX(2);
-  private final WPI_TalonFX rightFront = new WPI_TalonFX(3);
-  private final WPI_TalonFX rightBack = new WPI_TalonFX(4);
+    boolean True = true;
+    private final WPI_TalonFX leftFront = new WPI_TalonFX(1);
+    private final WPI_TalonFX leftBack = new WPI_TalonFX(2);
+    private final WPI_TalonFX rightFront = new WPI_TalonFX(3);
+    private final WPI_TalonFX rightBack = new WPI_TalonFX(4);
 
-  private final MotorControllerGroup m_leftSide = new MotorControllerGroup(leftBack, leftFront);
-  private final MotorControllerGroup m_rightSide = new MotorControllerGroup(rightBack, rightFront);
+    private final MotorControllerGroup m_leftSide = new MotorControllerGroup(leftBack, leftFront);
+    private final MotorControllerGroup m_rightSide = new MotorControllerGroup(rightBack, rightFront);
 
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftSide, m_rightSide);
+    private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftSide, m_rightSide);
 
-  private final CANSparkMax leftintake = new CANSparkMax(2, MotorType.kBrushless);
-  private final CANSparkMax rightintake = new CANSparkMax(7, MotorType.kBrushless);
+    private final CANSparkMax leftintake = new CANSparkMax(2, MotorType.kBrushless);
+    private final CANSparkMax rightintake = new CANSparkMax(7, MotorType.kBrushless);
 
-  private final MotorControllerGroup intake = new MotorControllerGroup(leftintake, rightintake);
+    private final MotorControllerGroup intake = new MotorControllerGroup(leftintake, rightintake);
 
-  private final Timer timer = new Timer();
+    private final Timer timer = new Timer();
 
-  private final XboxController m_stick = new XboxController(0);
+    private final XboxController xbox = new XboxController(0);
 
-  private final DoubleSolenoid dSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
-  private final Compressor compressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
-  
-  private final double Scale = 250, offset = -25;
-  private final AnalogPotentiometer potentiometer = new AnalogPotentiometer(0, Scale, offset);
+    private final DoubleSolenoid dSolenoidShifter = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+    private final DoubleSolenoid dSolenoidCool = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 2, 3);
+    private final Compressor compressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
 
-  private AHRS navx;  
-  
- public Value DSstatus;
- public boolean DSbool;
+    private final double Scale = 250, offset = -25;
+    private final AnalogPotentiometer potentiometer = new AnalogPotentiometer(0, Scale, offset);
 
-  public Robot() {
-      
-    try {
-      navx = new AHRS(SPI.Port.kMXP);
-    } catch (Exception e) {
-      System.out.println(e.getMessage());
+    private AHRS navx;
+
+    public Value coolingSolenoid;
+    public boolean DSbool;
+
+    public Robot() {
+
+        try {
+            navx = new AHRS(SPI.Port.kMXP);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        SmartDashboard.putBoolean("Solenoid Status(open = true): ", DSbool);
     }
-    SmartDashboard.putBoolean("Solenoid Status(open = true): ", DSbool);
-  }
 
-
-  @Override
-  public void robotInit() {
-    double psi = potentiometer.get();
-    if (psi <= 119) {
-      compressor.enableDigital();
-    } else if (psi > 119) {
-      compressor.disable();
+    @Override
+    public void robotInit() {
+        double psi = potentiometer.get();
+        if (psi <= 119) {
+            compressor.enableDigital();
+        } else if (psi > 119) {
+            compressor.disable();
+        }
     }
-  }
 
-  @Override
-  public void autonomousInit() {
-    navx.reset();
-    timer.reset();
-    timer.start();
-  }
+    @Override
+    public void autonomousInit() {
+        navx.reset();
+        timer.reset();
+        timer.start();
+    }
 
-  @Override
-   
-  public void autonomousPeriodic() {
-    double robotPitch = navx.getPitch();
+    @Override
+
+    public void autonomousPeriodic() {
+        double robotPitch = navx.getPitch();
+
+        m_robotDrive.isSafetyEnabled();
+
+        SmartDashboard.putData(navx);
+        SmartDashboard.putNumber("NAVXANGLE", robotPitch);
+
+        leftFront.setNeutralMode(NeutralMode.Brake);
+        leftBack.setNeutralMode(NeutralMode.Brake);
+        rightFront.setNeutralMode(NeutralMode.Brake);
+        rightBack.setNeutralMode(NeutralMode.Brake);
+
+        /*
+         * if (time > 0 && time < 4 ) {
+         * m_robotDrive.arcadeDrive(0, -0.50);
+         * 
+         * } else if (time < 6 && time > 4) {
+         * m_robotDrive.arcadeDrive(0.5, 0);
+         * 
+         * } else if (time < 8 && time > 6) {
+         * m_robotDrive.arcadeDrive(0, -0.5);
+         * 
+         * } else {
+         * m_robotDrive.arcadeDrive(0, 0);
+         * }
+         */
+
+        RobotAngle currentPitch = getPitch(robotPitch);
+        double fastLean = 0.40;
+        double slowLean = 0.20;
+        if (currentPitch == RobotAngle.Balanced) {
+            m_robotDrive.arcadeDrive(0, 0);
+        } else if (currentPitch == RobotAngle.Forward) {
+            m_robotDrive.arcadeDrive(0, fastLean);
+        } else if (currentPitch == RobotAngle.leaningForward) {
+            m_robotDrive.arcadeDrive(0, slowLean);
+        } else if (currentPitch == RobotAngle.Backward) {
+            m_robotDrive.arcadeDrive(0, -fastLean);
+        } else if (currentPitch == RobotAngle.leaningBackward) {
+            m_robotDrive.arcadeDrive(0, -slowLean);
+        }
+    }
+
+    enum RobotAngle {
+        Forward,
+        leaningForward,
+        Balanced,
+        leaningBackward,
+        Backward,
+        unset
+    }
+
+    // 0.7 1.5 -0.25 nuet
+    // 16 back
+    // -13.25 front
+
+    double tolerance = 2;
+    double tiltBack = 15;
+    double tiltFwd = -12;
+    double bal = 0;
+
+    public RobotAngle getPitch(double robotPitch) {
+        RobotAngle result = RobotAngle.unset;
+        if (Math.abs(robotPitch) < tolerance) {
+            result = RobotAngle.Balanced;
+        } else if (robotPitch > tiltBack) {
+            result = RobotAngle.Backward;
+        } else if (robotPitch < tiltBack && robotPitch > tolerance) {
+            result = RobotAngle.leaningBackward;
+        } else if (robotPitch > tiltFwd && robotPitch < -tolerance) {
+            result = RobotAngle.leaningForward;
+        } else if (robotPitch < tiltFwd) {
+            result = RobotAngle.Forward;
+        }
+        SmartDashboard.putString("Robot Pitch", result.toString());
+        return result;
+    }
+
+    @Override
+    public void teleopInit() {
+        timer.reset();
+        timer.start();
+    }
     
-    m_robotDrive.isSafetyEnabled();
-
-    SmartDashboard.putData(navx);
-    SmartDashboard.putNumber("NAVXANGLE", robotPitch);
-
-    leftFront.setNeutralMode(NeutralMode.Brake);
-    leftBack.setNeutralMode(NeutralMode.Brake);
-    rightFront.setNeutralMode(NeutralMode.Brake);
-    rightBack.setNeutralMode(NeutralMode.Brake);
-
-
-
-/*
-  if (time > 0 && time < 4 ) { 
-    m_robotDrive.arcadeDrive(0, -0.50);
-
-  } else if (time < 6 && time > 4) { 
-    m_robotDrive.arcadeDrive(0.5, 0);
-
-  } else if (time < 8 && time > 6) {
-    m_robotDrive.arcadeDrive(0, -0.5);
-
-  } else {
-    m_robotDrive.arcadeDrive(0, 0);
-  } */
-  
-  RobotAngle currentPitch = getPitch(robotPitch);
-  double fastLean = 0.40;
-  double slowLean = 0.20;
-  if (currentPitch == RobotAngle.Balanced) {
-    m_robotDrive.arcadeDrive(0,0);
-  }
-  else if (currentPitch == RobotAngle.Forward){
-    m_robotDrive.arcadeDrive(0, fastLean);
-  }
-  else if (currentPitch == RobotAngle.leaningForward) {
-    m_robotDrive.arcadeDrive(0, slowLean);
-  }
-  else if (currentPitch == RobotAngle.Backward){
-    m_robotDrive.arcadeDrive(0, -fastLean);
-  }
-  else if (currentPitch == RobotAngle.leaningBackward){
-    m_robotDrive.arcadeDrive(0, -slowLean);
-  }
-  }
-
-  enum RobotAngle {
-    Forward,
-    leaningForward,
-    Balanced,
-    leaningBackward,
-    Backward,
-    unset
-  }
-
-  // 0.7 1.5 -0.25 nuet
-  // 16 back
-  // -13.25 front
-
-  double tolerance = 2;
-  double tiltBack = 15;
-  double tiltFwd = -12;
-  double bal = 0;
-
-  public RobotAngle getPitch(double robotPitch) {
-    RobotAngle result = RobotAngle.unset;
-    if (Math.abs(robotPitch) < tolerance) {
-      result = RobotAngle.Balanced;
-    } else if (robotPitch > tiltBack) {
-      result = RobotAngle.Backward;
-    } else if (robotPitch < tiltBack && robotPitch > tolerance) {
-      result = RobotAngle.leaningBackward;
-    } else if (robotPitch > tiltFwd && robotPitch < -tolerance) {
-      result = RobotAngle.leaningForward;
-    } else if (robotPitch < tiltFwd) {
-      result = RobotAngle.Forward;
-    }
-    SmartDashboard.putString("Robot Pitch", result.toString());
-    return result;
-  }
-
-@Override
-public void teleopInit() {
-  timer.reset();
-  timer.start();
-}
-
-  @Override
-  public void teleopPeriodic() {    
-    double time = timer.get();
-    DSstatus = dSolenoid.get();
-       
-    /*
-     * if time >= 15 sec
-     * get dsol state
-     * switch 
-     * then switch back
-     * reset the timer
-     */
-  if(time >= 15){
-    if(DSstatus==Value.kReverse){
-      dSolenoid.set(Value.kForward);
-      dSolenoid.set(Value.kReverse);
-    }else if(DSstatus==Value.kForward){
-      dSolenoid.set(Value.kReverse);
-      dSolenoid.set(Value.kForward);
-    }
-    timer.reset();
-  }
-
-  //if ^^^^^^^^^^^^ no work try dis below \/
-    /*
-  if (DSstatus == Value.kForward ) {
-    DSbool = true;
-  } else if (DSstatus == Value.kReverse ) {
-    DSbool = false;
-  }
-
-if(time >= 15){
-  if (DSbool = true){
-    dSolenoid.set(Value.kReverse);
-    dSolenoid.set(Value.kForward);
-  } else if (DSbool = false) {
-    dSolenoid.set(Value.kForward);
-    dSolenoid.set(Value.kReverse);
-  }
-  timer.reset();
-}
-*/
-    
-    double l_speed = m_leftSide.get();
-    double r_speed = m_rightSide.get();
-    double pItch = navx.getPitch();
-    SmartDashboard.putData(navx);
-    SmartDashboard.putNumber("NAVXANGLE", pItch);
-    SmartDashboard.putNumber("Speed Left", l_speed);
-    SmartDashboard.putNumber("SPeed Right", r_speed);
-
-    leftFront.setNeutralMode(NeutralMode.Brake);
-    leftBack.setNeutralMode(NeutralMode.Brake);
-    rightFront.setNeutralMode(NeutralMode.Brake);
-    rightBack.setNeutralMode(NeutralMode.Brake);
-    m_robotDrive.arcadeDrive(m_stick.getRawAxis(4) * 0.8, m_stick.getRawAxis(1) * 0.8);
-
-    if (m_stick.getRawButton(1) == true) {
-      dSolenoid.set(Value.kForward);
-    } else if (m_stick.getRawButton(2) == true) {
-      dSolenoid.set(Value.kReverse);
+    private void pulsePiston(double teleopTime) {
+        int pulseFreq = 15;
+        int pulseDuration = 1;
+        if (timer.get() % pulseFreq < pulseDuration) {
+            dSolenoidCool.set(Value.kForward);
+        } else {
+            dSolenoidCool.set(Value.kReverse);
+        }
     }
 
-    if (m_stick.getRawButton(6) == true) {
-      rightintake.set(-1);
-      leftintake.set(1);
-    } else if (m_stick.getRawButton(5) == true) {
-      leftintake.set(-1);
-      rightintake.set(1);
-    } else {
-      leftintake.set(0);
-      rightintake.set(0);
+    @Override
+    public void teleopPeriodic() {
+        double teleopTime = timer.get();
+        
+
+        pulsePiston(teleopTime);
+
+        double l_speed = m_leftSide.get();
+        double r_speed = m_rightSide.get();
+        double pItch = navx.getPitch();
+        SmartDashboard.putData(navx);
+        SmartDashboard.putNumber("NAVXANGLE", pItch);
+        SmartDashboard.putNumber("Speed Left", l_speed);
+        SmartDashboard.putNumber("SPeed Right", r_speed);
+
+        leftFront.setNeutralMode(NeutralMode.Brake);
+        leftBack.setNeutralMode(NeutralMode.Brake);
+        rightFront.setNeutralMode(NeutralMode.Brake);
+        rightBack.setNeutralMode(NeutralMode.Brake);
+        m_robotDrive.arcadeDrive(xbox.getRawAxis(4) * 0.8, xbox.getRawAxis(1) * 0.8);
+
+        if (xbox.getAButton() == true) {
+            dSolenoidShifter.set(Value.kForward);
+        } else if (xbox.getBButton() == true) {
+            dSolenoidShifter.set(Value.kReverse);
+        }
+
+        if (xbox.getRawButton(6) == true) {
+            rightintake.set(-1);
+            leftintake.set(1);
+        } else if (xbox.getRawButton(5) == true) {
+            leftintake.set(-1);
+            rightintake.set(1);
+        } else {
+            leftintake.set(0);
+            rightintake.set(0);
+        }
     }
-  }
+
 }
