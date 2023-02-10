1a1a90307df039abc57670d5a46ce1567dfd7817
Gryph 2634 <gryphons2634@gmail.com>
2023-02-09 21:36:23 -0500

    navx curve

--- Timed/TimedRizz/src/main/java/frc/robot/Robot.java
+++ Timed/TimedRizz/src/main/java/frc/robot/Robot.java
@@ -22,27 +22,36 @@ import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.SPI;
 
 public class Robot extends TimedRobot {
-
-    private final WPI_TalonFX leftFront = new WPI_TalonFX(1);
-    private final WPI_TalonFX leftBack = new WPI_TalonFX(2);
-    private final WPI_TalonFX rightFront = new WPI_TalonFX(3);
-    private final WPI_TalonFX rightBack = new WPI_TalonFX(4);
+    /*
+     * private final WPI_TalonFX leftFront = new WPI_TalonFX(1);
+     * private final WPI_TalonFX leftBack = new WPI_TalonFX(2);
+     * private final WPI_TalonFX rightFront = new WPI_TalonFX(3);
+     * private final WPI_TalonFX rightBack = new WPI_TalonFX(4);
+     */
+    private final CANSparkMax leftFront = new CANSparkMax(10, MotorType.kBrushless);
+    private final CANSparkMax leftBack = new CANSparkMax(18, MotorType.kBrushless);
+    private final CANSparkMax rightFront = new CANSparkMax(17, MotorType.kBrushless);
+    private final CANSparkMax rightBack = new CANSparkMax(4, MotorType.kBrushless);
 
     private final MotorControllerGroup m_leftSide = new MotorControllerGroup(leftBack, leftFront);
     private final MotorControllerGroup m_rightSide = new MotorControllerGroup(rightBack, rightFront);
 
     private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftSide, m_rightSide);
 
-    private final CANSparkMax leftIntake = new CANSparkMax(2, MotorType.kBrushless);
-    private final CANSparkMax rightIntake = new CANSparkMax(7, MotorType.kBrushless);
+    // private final CANSparkMax leftIntake = new CANSparkMax(2,
+    // MotorType.kBrushless);
+    // private final CANSparkMax rightIntake = new CANSparkMax(7,
+    // MotorType.kBrushless);
 
-    private final MotorControllerGroup intakeMotors = new MotorControllerGroup(leftIntake, rightIntake);
+    // private final MotorControllerGroup intakeMotors = new
+    // MotorControllerGroup(leftIntake, rightIntake);
 
     private final Timer timer = new Timer();
 
@@ -74,7 +83,7 @@ public class Robot extends TimedRobot {
         int psiCap = 117;
         if (currentPsi <= psiCap) {
             compressor.enableDigital();
-        } else if (currentPsi > 119 ) {
+        } else if (currentPsi > 119) {
             compressor.disable();
         }
     }
@@ -86,16 +95,24 @@ public class Robot extends TimedRobot {
         timer.start();
     }
 
-    public void setMotorsNeutral () {
-        leftFront.setNeutralMode(NeutralMode.Brake);
-        leftBack.setNeutralMode(NeutralMode.Brake);
-        rightFront.setNeutralMode(NeutralMode.Brake);
-        rightBack.setNeutralMode(NeutralMode.Brake);
+    public void setMotorsNeutral() {
+        /*
+         * leftFront.setNeutralMode(NeutralMode.Brake);
+         * leftBack.setNeutralMode(NeutralMode.Brake);
+         * rightFront.setNeutralMode(NeutralMode.Brake);
+         * rightBack.setNeutralMode(NeutralMode.Brake);
+         */
+
+        leftFront.setIdleMode(IdleMode.kBrake);
+        leftBack.setIdleMode(IdleMode.kBrake);
+        rightFront.setIdleMode(IdleMode.kBrake);
+        rightBack.setIdleMode(IdleMode.kBrake);
     }
 
-    public void balanceRobot() {
-        double robotPitch = navx.getPitch();
+    double previousPitch = 0;
 
+    public void balanceRobot(double robotPitch) {
+        
         RobotAngle currentPitchState = getPitchState(robotPitch);
         double fastLeanSpeed = 0.40;
         double slowLeanSpeed = 0.20;
@@ -109,7 +126,7 @@ public class Robot extends TimedRobot {
         } else if (currentPitchState == RobotAngle.leaningForward) {
             if (robotPitch > previousPitch) {
                 m_robotDrive.arcadeDrive(0, slowLeanSpeed);
-            } else if (robotPitch <= previousPitch){
+            } else if (robotPitch <= previousPitch) {
                 m_robotDrive.arcadeDrive(0, 0);
             }
 
@@ -119,14 +136,13 @@ public class Robot extends TimedRobot {
         } else if (currentPitchState == RobotAngle.leaningBackward) {
             if (robotPitch < previousPitch) {
                 m_robotDrive.arcadeDrive(0, -slowLeanSpeed);
-            } else if (robotPitch >= previousPitch){
+            } else if (robotPitch >= previousPitch) {
                 m_robotDrive.arcadeDrive(0, 0);
-            }        
+            }
         }
         previousPitch = robotPitch;
     }
-
-    double previousPitch = navx.getPitch();
+/*
     @Override
     public void autonomousPeriodic() {
         double robotPitch = navx.getPitch();
@@ -138,7 +154,7 @@ public class Robot extends TimedRobot {
 
         balanceRobot();
     }
-
+ */
     enum RobotAngle {
         Forward,
         leaningForward,
@@ -168,7 +184,7 @@ public class Robot extends TimedRobot {
 
         } else if (robotPitch < tiltBack && robotPitch > tolerance) {
             result = RobotAngle.leaningBackward;
-            
+
         } else if (robotPitch > tiltFwd && robotPitch < -tolerance) {
             result = RobotAngle.leaningForward;
 
@@ -183,8 +199,11 @@ public class Robot extends TimedRobot {
     public void teleopInit() {
         timer.reset();
         timer.start();
+        navx.reset();
+        m_leftSide.setInverted(false);
+        m_rightSide.setInverted(false);
     }
-    
+
     private void pulsePiston(double teleopTime) {
         int pulseFreq = 15;
         int pulseDuration = 1;
@@ -198,59 +217,62 @@ public class Robot extends TimedRobot {
     @Override
     public void teleopPeriodic() {
         double teleopTime = timer.get();
-        
-        pulsePiston(teleopTime);
+
+        // pulsePiston(teleopTime);
 
         double l_speed = m_leftSide.get();
         double r_speed = m_rightSide.get();
-        double pItch = navx.getPitch();
+        double pitchNavx = navx.getPitch();
         SmartDashboard.putData(navx);
-        SmartDashboard.putNumber("NAVXANGLE", pItch);
+        SmartDashboard.putNumber("NAVXANGLE", pitchNavx);
         SmartDashboard.putNumber("Speed Left", l_speed);
         SmartDashboard.putNumber("SPeed Right", r_speed);
 
         setMotorsNeutral();
 
         m_robotDrive.arcadeDrive(xbox.getRawAxis(4) * 0.8, xbox.getRawAxis(1) * 0.8);
-// deadzone
-       // if (xbox.getRaw)
+        // deadzone
+        // if (xbox.getRaw)
 
         if (xbox.getAButton() == true) {
             dSolenoidShifter.set(Value.kForward);
         } else if (xbox.getBButton() == true) {
             dSolenoidShifter.set(Value.kReverse);
         }
-
-        if (xbox.getRightBumper() == true) {
-            rightIntake.set(-1);
-            leftIntake.set(1);
-        } else if (xbox.getLeftBumper() == true) {
-            leftIntake.set(-1);
-            rightIntake.set(1);
-        } else {
-            leftIntake.set(0);
-            rightIntake.set(0);
-        }
-
+        /*
+         * if (xbox.getRightBumper() == true) {
+         * rightIntake.set(-1);
+         * leftIntake.set(1);
+         * } else if (xbox.getLeftBumper() == true) {
+         * leftIntake.set(-1);
+         * rightIntake.set(1);
+         * } else {
+         * leftIntake.set(0);
+         * rightIntake.set(0);
+         * }
+         */
 
         if (xbox.getXButtonPressed() == true) {
             xButtonPressed = !xButtonPressed;
         }
 
-        double deadzone = 0.01;
-        if (xbox.getRawAxis(4) > deadzone || 
-            xbox.getRawAxis(4) < -deadzone && 
-            xbox.getRawAxis(1) > deadzone || 
-            xbox.getRawAxis(1) < -deadzone) 
-            {
-                xButtonPressed = false;
+        double deadzone = 0.5;
+        if (xbox.getRawAxis(4) > deadzone ||
+                xbox.getRawAxis(4) < -deadzone &&
+                        xbox.getRawAxis(1) > deadzone
+                ||
+                xbox.getRawAxis(1) < -deadzone) {
+            xButtonPressed = false;
         }
 
-        if(xButtonPressed == true){
-            balanceRobot();
+        if (xButtonPressed == true) {
+            balanceRobot(pitchNavx);
         }
-    }   
+        SmartDashboard.putBoolean("Balance mode: ", xButtonPressed);
+        if (xbox.getYButton() == true) {
+            navx.reset();
+        }
+    }
 
     boolean xButtonPressed = false;
-
 }
