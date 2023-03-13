5df47a1f6867290c12746800bc32527911c0a9a2
Crimson <121061785+Adri-nK@users.noreply.github.com>
2023-03-12 22:01:11 -0400

    navx and function updateed

--- autonomousCoding/src/main/java/frc/robot/Robot.java
+++ autonomousCoding/src/main/java/frc/robot/Robot.java
@@ -9,51 +9,58 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SPI;
 
 public class Robot extends TimedRobot {
- 
-  public final WPI_TalonFX leftFront= new WPI_TalonFX(1);
-  public final WPI_TalonFX rightFront= new WPI_TalonFX(3);
-  public final WPI_TalonFX leftBack= new WPI_TalonFX(2);
-  public final WPI_TalonFX rightBack= new WPI_TalonFX(4); 
-  double arcadeDrive_GearRatio = 0; //figure out note: will change for swerver drive
 
+  private AHRS navx = new AHRS(SPI.Port.kMXP);
+ 
+  final WPI_TalonFX leftFront= new WPI_TalonFX(1);
+  final WPI_TalonFX rightFront= new WPI_TalonFX(3);
+  final WPI_TalonFX leftBack= new WPI_TalonFX(2);
+  final WPI_TalonFX rightBack= new WPI_TalonFX(4); 
   final MotorControllerGroup m_leftSide = new MotorControllerGroup(leftBack, leftFront);
   final MotorControllerGroup m_rightSide = new MotorControllerGroup(rightBack, rightFront);
-
   final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftSide, m_rightSide);
+  double arcadeDrive_GearRatio = 0; //figure out note: will change for swerver drive
+  //^^arcade setup
 
   public final WPI_TalonFX topRight= new WPI_TalonFX(6); 
-  public final WPI_TalonFX topLeft= new WPI_TalonFX(5); 
+  public final WPI_TalonFX topLeft= new WPI_TalonFX(5);
+  double armSpeed = 0.4;
+  double reArmSpeed = -0.4;
+  private final DifferentialDrive topsDrive = new DifferentialDrive(topLeft, topRight); 
   double armLift_GearRatio = Math.pow(6,2);
+  //^^TankArm setup
 
-  private final DifferentialDrive topsDrive = new DifferentialDrive(topLeft, topRight);
-
-  // push arm ratio: 12:1
+  final WPI_TalonFX armTalonExtenstion = new WPI_TalonFX(7);
+  double armTalonExtenstion_GearRatio = 12;
+  //^^extend arm setup
 
   final XboxController xBoxCont = new XboxController(0);
 
-  final double kp = 0.5;
-  final double ki = 0.05;
-  final double kd = 0.05;
-  boolean xButtonPressed = false;
-
-  double armSpeed = 0.4;
-  double reArmSpeed = -0.4;
   double diameterRizzArcadeWheels = 6.5;
   double radiusRizzArcadeWheels = diameterRizzArcadeWheels/2; //of wheel in inchs
   double countsPerRevTalonFX = 2048;
   
-  PIDController drive = new PIDController(kp, ki, kd);   
+  final double kp = 0.5;
+  final double ki = 0.05;
+  final double kd = 0.05;
+  final double kpPitch = 0.5; 
+  final double kpYaw = 0.5;
+  // may need to be set by user in function call
+
+  PIDController drive = new PIDController(kp, ki, kd); // may need to spearate for turnings
+  PIDController pidPitch = new PIDController(kpPitch, ki, kd);
+  PIDController pidYaw = new PIDController(kpYaw, ki, kd);
 
 public double encoderToFeet(double radius, double countsPerRev, double encoderValue, double gearRatio){
   double circumferenceOfWheel = 2*Math.PI*radius;
   double countsPerInch = countsPerRev / circumferenceOfWheel / gearRatio;
   double inches = encoderValue / countsPerInch;
   double feet = inches / 12;
-
   // in other words: double Feet = (encoderValue/(countsPerRev/(2*Math.PI*radius)))/12;
-
   return feet;
 }
 
@@ -65,11 +72,19 @@ public double encoderToDegrees(double countsPerRev, double encoderValue, double 
 }
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-public void driveFwd_or_Bwd_AUTONOMOUS(double targetDistanceFeet, double tolerance) {
+public void drive_AUTONOMOUS(double targetDistanceFeet, double tolerance, boolean currentMotorTurn) {
     resetEncoder();
     double output;
-    double currentDistance = leftFront.getSelectedSensorPosition();
+    double currentDistance;
 
+    if (currentMotorTurn == false){
+      currentDistance = leftFront.getSelectedSensorPosition();
+    } else if (currentMotorTurn == true) {
+      currentDistance = rightFront.getSelectedSensorPosition();
+    } else {
+      currentDistance = leftFront.getSelectedSensorPosition();
+    }
+    
     if (Math.abs(targetDistanceFeet - currentDistance) > tolerance) {
       output = drive.calculate(currentDistance, targetDistanceFeet);
       output = Math.max(-1, Math.min(1, output));
@@ -80,10 +95,19 @@ public void driveFwd_or_Bwd_AUTONOMOUS(double targetDistanceFeet, double toleran
     }
   }
 
-public void driveFwd_or_Bwd_NORMAL(double targetDistanceFeet, double tolerance) {
+public void drive_NORMAL(double targetDistanceFeet, double tolerance, boolean currentMotorTurn) {
    resetEncoder();
    double output;
-   double currentDistance = leftFront.getSelectedSensorPosition();
+   double currentDistance;
+
+   if (currentMotorTurn == false){
+    currentDistance = leftFront.getSelectedSensorPosition();
+  } else if (currentMotorTurn == true) {
+    currentDistance = rightFront.getSelectedSensorPosition();
+  } else {
+    currentDistance = leftFront.getSelectedSensorPosition();
+  }
+
    if (Math.abs(targetDistanceFeet - currentDistance) > tolerance) {
      output = drive.calculate(currentDistance, targetDistanceFeet);
      output = Math.max(-1, Math.min(1, output));
@@ -94,6 +118,7 @@ public void driveFwd_or_Bwd_NORMAL(double targetDistanceFeet, double tolerance) 
    }
  }
 
+/*
 public void driveTurn_leftOrRight_AUTONOMOUS(double targetDistanceDegrees, double tolerance) {
   resetEncoder();
   double output;
@@ -121,6 +146,7 @@ public void driveTurn_leftOrRight_NORMAL(double targetDistanceDegrees, double to
    m_robotDrive.stopMotor();
  }
 }
+ */
 
 public void limitArmRotation() {
   if (topRight.getSelectedSensorPosition() >= 90){ //change degrees
@@ -130,14 +156,41 @@ public void limitArmRotation() {
     topsDrive.tankDrive(reArmSpeed, armSpeed);
   } 
 }
+
+public void autoBalance_NORMAL() {
+  double outputPitch=0;
+  double outputYaw=0;
+  double currentPitch;
+  double currentYaw;
+  double targetAnglePitch = 0;
+  double targetAngleYaw = 0;
+  double tolerance = 5;
+  
+  navx.reset(); // recalibration may take 10 seconds!!!!!!!!!!
+  currentPitch = navx.getPitch();
+  currentYaw = navx.getYaw();
+
+  if (Math.abs(targetAnglePitch - currentPitch) > tolerance) {
+    outputPitch = pidPitch.calculate(currentPitch, targetAnglePitch);
+    outputPitch = Math.max(-1, Math.min(1, outputPitch));
+  }
+  if (Math.abs(targetAngleYaw - currentYaw) > tolerance) {
+    outputYaw = pidYaw.calculate(currentYaw, targetAngleYaw);
+    outputYaw = Math.max(-1, Math.min(1, outputYaw));
+  }
+  m_robotDrive.arcadeDrive(outputPitch, outputYaw);
+}
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 public void setMotorsNeutral() {
     
-    leftFront.setNeutralMode(NeutralMode.Brake);
-    leftBack.setNeutralMode(NeutralMode.Brake);
-    rightFront.setNeutralMode(NeutralMode.Brake);
-    rightBack.setNeutralMode(NeutralMode.Brake);
+  leftFront.setNeutralMode(NeutralMode.Brake);
+  leftBack.setNeutralMode(NeutralMode.Brake);
+  rightFront.setNeutralMode(NeutralMode.Brake);
+  rightBack.setNeutralMode(NeutralMode.Brake);
+  topLeft.setNeutralMode(NeutralMode.Brake);
+  topRight.setNeutralMode(NeutralMode.Brake);
+  armTalonExtenstion.setNeutralMode(NeutralMode.Brake);
   }
   
 public void resetEncoder(){
@@ -166,27 +219,34 @@ public void robotInit() {
   resetEncoder();
   }
 
-  @Override
+@Override
 public void robotPeriodic() {
   //limitArmRotation();
 
-  // print oout encoder status
   SmartDashboard.putNumber("topRight.Encoder: ", topRight.getSelectedSensorPosition());
   SmartDashboard.putNumber("leftFront.Encoder: ", leftFront.getSelectedSensorPosition());
   SmartDashboard.putNumber("rightFront.Encoder: ", rightFront.getSelectedSensorPosition());
+  //^^encoder
+  SmartDashboard.putNumber("navx getPitch: ", navx.getPitch());
+  SmartDashboard.putNumber("navx getYaw: ", navx.getYaw());
+  SmartDashboard.putNumber("navx getRoll: ", navx.getRoll());
+  SmartDashboard.putNumber("navx getAngle: ", navx.getAngle());
+  //^^navx Angles
 
   }
   
   @Override
   public void autonomousInit() {
-    driveFwd_or_Bwd_AUTONOMOUS(12, 1);
-    driveFwd_or_Bwd_AUTONOMOUS(-12, 1);
-    driveTurn_leftOrRight_AUTONOMOUS(180, 1);
-    driveTurn_leftOrRight_AUTONOMOUS(-180, 1);
+    drive_AUTONOMOUS(12, 1, false); //fwd 12 feet
+    drive_AUTONOMOUS(-12, 1, false); //Bakwd 12 feet
+    drive_AUTONOMOUS(180, 1, true); //turn 180
+    drive_AUTONOMOUS(-180, 1, true); //turn back 180
   }
 
   @Override
   public void autonomousPeriodic() {
+    limitArmRotation();
+    autoBalance_NORMAL();
     /*
      * align with starting node
      * rais arm
@@ -209,5 +269,22 @@ public void robotPeriodic() {
     m_robotDrive.arcadeDrive(xBoxCont.getRawAxis(4) * 0.8, xBoxCont.getRawAxis(1) * 0.8);
     topsDrive.tankDrive(xBoxCont.getLeftTriggerAxis(), -xBoxCont.getLeftTriggerAxis());
     topsDrive.tankDrive(-xBoxCont.getLeftTriggerAxis(), xBoxCont.getLeftTriggerAxis());
+    /*
+    if (xBoxCont.getLeftTriggerAxis() >= 0.5) {
+      topsDrive.tankDrive(armSpeed,-armSpeed);
+  }else if (xBoxCont.getRightTriggerAxis() >= 0.5) {
+      topsDrive.tankDrive(-armSpeed,armSpeed);
+  }else {
+      topsDrive.tankDrive(0,0);
+  } 
+  */
+
+    if (xBoxCont.getRightBumper() == true){
+      armTalonExtenstion.set(.30);
+  } else if (xBoxCont.getLeftBumper() == true){
+      armTalonExtenstion.set(-0.30);
+  } else {
+      armTalonExtenstion.set(0);
+  }
   }
 }
