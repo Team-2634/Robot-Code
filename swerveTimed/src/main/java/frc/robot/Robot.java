d09e9901580d11b916d6b67b354b146bdb6ca57a
team2634 <gryphons2634@gmail.com>
2023-03-16 15:46:43 -0400

    module positions

--- swerveTimed/src/main/java/frc/robot/Robot.java
+++ swerveTimed/src/main/java/frc/robot/Robot.java
@@ -1,6 +1,7 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
+// Victor was here
 
 package frc.robot;
 
@@ -96,10 +97,10 @@ public class Robot extends TimedRobot {
 
 */
   //define location of modules/wheels
-  Translation2d m_frontLeftLocation = new Translation2d(0.381, 0.381);
-  Translation2d m_frontRightLocation = new Translation2d(0.381, -0.381);
-  Translation2d m_backLeftLocation = new Translation2d(-0.381, 0.381);
-  Translation2d m_backRightLocation = new Translation2d(-0.381, -0.381);
+  Translation2d m_frontLeftLocation = new Translation2d(0.340, 0.285);
+  Translation2d m_frontRightLocation = new Translation2d(0.340, -0.285);
+  Translation2d m_backLeftLocation = new Translation2d(-0.340, 0.285);
+  Translation2d m_backRightLocation = new Translation2d(-0.340, -0.285);
 
   //feed the four modules into a kinematics function
   SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(
