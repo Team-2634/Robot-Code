f9145fe1c1b98d0de988a64a52a612f9271f8ae6
team2634 <gryphons2634@gmail.com>
2023-03-16 15:23:37 -0400

    thingy

---
+++ RizzSwerve/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,45 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Robot extends TimedRobot {
+
+  //Constants vvvvv
+
+  //constants ^^^^^
+
+  // our functions vvvvvv
+
+  public void swerveDrive() {
+    
+  }
+
+  //execution Functions vvvvv
+  @Override
+  public void robotInit() {
+  }
+
+  @Override
+  public void robotPeriodic() {
+
+  }
+  
+  @Override
+  public void autonomousInit() {
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    
+  }
+
+  @Override
+  public void teleopInit() {
+
+  }
+
+  @Override
+  public void teleopPeriodic() {
+
+  }
+}
