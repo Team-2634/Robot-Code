3d47fe8e1a9d03f0f27fed65c4c2cd7fb4b84f3f
Victor Galut <galutvic@gmail.com>
2024-03-28 21:54:56 -0400

    converting long math into variables

--- RizzSwerve-Imported/src/main/java/frc/robot/TeleopHelper.java
+++ RizzSwerve-Imported/src/main/java/frc/robot/TeleopHelper.java
@@ -62,14 +62,25 @@ public class TeleopHelper {
     }
 
     public double calculateArmAngle(){
-        double botToSpeakerDist = (Constants.floorToTarget - Constants.floorToLimelight) / (Math.tan((Constants.limelightAngle + limelight.ty) * Constants.degToRad));
-        double limelightToSpeakerAngle = Math.atan((Constants.targetToSpeaker + Constants.floorToTarget - Constants.floorToLimelight) / botToSpeakerDist);
+
+        double floorToSpeaker = Constants.targetToSpeaker + Constants.floorToTarget;
+        
+        double armToTag = Constants.floorToTarget - Constants.floorToLimelight;
+        double limelightAngleToTarget = Constants.limelightAngle + limelight.ty;
+        
+        
+        double botToSpeakerVertical = floorToSpeaker - Constants.floorToLimelight;
+        double botToSpeakerHorizontal = armToTag / Math.tan(limelightAngleToTarget * Constants.degToRad);
+
+        double limelightToSpeakerAngle = Math.atan(botToSpeakerVertical / botToSpeakerHorizontal);
+
         // double limelightToSpeakerLength = botToSpeakerDist / Math.cos(limelightToSpeakerAngle);
-        double limelightToSpeakerLength = Math.sqrt(Math.pow(botToSpeakerDist, 2) + Math.pow(Constants.floorToTarget + Constants.targetToSpeaker - Constants.floorToLimelight, 2));
+        double limelightToSpeakerLength = Math.sqrt(Math.pow(botToSpeakerHorizontal, 2) + Math.pow(botToSpeakerVertical, 2));
+
         double angleThree = Math.asin((Constants.armLength * Math.sin(Constants.shooterToSpeakerAngle)) / limelightToSpeakerLength);
         double angleFour = (180 * Constants.degToRad) - angleThree - (55 * Constants.degToRad); // Find missing angle
         double angleArm = (180 * Constants.degToRad) - limelightToSpeakerAngle - angleFour;
-        return angleArm - (55 * Constants.degToRad);
+        return angleArm - (55 * Constants.degToRad) - 0.35;
     }
 
     public void shoot(double input) {
