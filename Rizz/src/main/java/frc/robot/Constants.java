96775cbf1d8f0798cf10eaa0ea0fff480656856b
Lordquackles865 <141074208+Lordquackles865@users.noreply.github.com>
2025-10-06 19:30:34 -0400

    Update Constants.java

--- Rizz/src/main/java/frc/robot/Constants.java
+++ Rizz/src/main/java/frc/robot/Constants.java
@@ -111,8 +111,8 @@ public class Constants {
     public final static double feederDistanceOffset1st = 2634; //In metres y distance from start to wall.
 
     //controller settings (robot speed)
-    public final static double XdriveSensitivity = 1;
-    public final static double YdriveSensitivity = 1; 
+    public final static double XdriveSensitivity = 0.75;
+    public final static double YdriveSensitivity = 0.75; 
     public final static double turningSensitivity = 1.0;
     public final static double maxSpeedMpS = 0.45; 
 
