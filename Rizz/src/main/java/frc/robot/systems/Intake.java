e6bf49a88bb797787f9340eff48e9b472f84d8d0
robotics-blip <robotics@chamnation.com>
2026-02-12 15:18:13 -0500

    Update Intake.java

--- Rizz/src/main/java/frc/robot/systems/Intake.java
+++ Rizz/src/main/java/frc/robot/systems/Intake.java
@@ -1,19 +1,16 @@
+package frc.robot;
 
-package frc.robot.systems;
-
-import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.Constants;
-import edu.wpi.first.wpilibj.smartdashboard.*;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.drive.RobotDriveBase.MotorType;
-import edu.wpi.first.wpilibj.motorcontrol.*;
+import frc.robot.systems.*;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-public class Intake {
 
-public final PWMSparkMax intakeMotor = new PWMSparkMax(1);
+public  class Intake {
 
+   public PWMSparkMax intakeMotor = new PWMSparkMax(1);
 
-    
+    public void spin() {
+        intakeMotor.set(1);
+    }
 
-    
+    //example: intake.spin()
+
 }
