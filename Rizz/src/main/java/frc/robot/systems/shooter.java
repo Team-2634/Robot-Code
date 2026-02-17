f808124b43d73714daff840df2b0aa615e186a91
LordQuackles865 <redswimmer558e@gmail.com>
2026-02-17 16:03:48 -0500

    Update shooter.java

--- Rizz/src/main/java/frc/robot/systems/shooter.java
+++ Rizz/src/main/java/frc/robot/systems/shooter.java
@@ -6,8 +6,8 @@ import frc.robot.Constants;
 
 public class shooter {
 
-    public static PWMSparkMax shooterMotor1 = new PWMSparkMax(2);
-    public static PWMSparkMax shooterMotor2 = new PWMSparkMax(3);
+    public static PWMSparkMax shooterMotor1 = new PWMSparkMax(3);
+    public static PWMSparkMax shooterMotor2 = new PWMSparkMax(4);
 
     public static void shooterOn() {
         shooterMotor1.set(Constants.shooterMotor1Speed);
